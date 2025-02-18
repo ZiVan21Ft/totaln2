7db90266e3e91d5afa81f5d7f693492b2850d89f
ZiVan21Ft <90625862+ZiVan21Ft@users.noreply.github.com>
2025-02-19 00:11:35 +0630

    Update name.js

--- name.js
+++ name.js
@@ -1,20 +1,17 @@
 function greet(a,b,c){
   //sum ab
   var sum = parseInt(a, 10);
-  var suma = (sum/59) * 60;
-
   var sumb = parseInt(b, 10);
-  var asrb = (sumb/59) * 60;
-  
-  //contain.textContent = "The result is"+""+ sum;
-  //sum c
   var sumc = parseInt(c, 10);
+  
+
+  
+  var suma = (sum/59) * 60;
+  var asrb = (sumb/59) * 60;
   var asrc = (sumc/55) * 60;
+  
   const total_num = Math.trunc(suma) + Math.trunc(asrb) + Math.trunc(asrc);
-  //document.getElementById("contain").innerText = "Your's total mark is"+ " "+ total_num+".";
-  //alert(sum);
-  //document.write(sum);
-  //document.write("hello"+firstname+""+secondname);
+  
   if(isNaN(total_num)){
     document.querySelector("#contain").textContent = "The result is none."
   } else {
